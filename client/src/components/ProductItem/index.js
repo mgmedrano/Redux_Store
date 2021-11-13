59b0d44ba9d8bf2a689f6f8bb354ee251a8c47c6
Michael Medrano <mgmedrano@outlook.com>
2021-11-13 15:53:37 -0800

    Updated Component to import redux to use state and dispatch

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
-import { useStoreContext } from "../../utils/GlobalState";
+// import { useStoreContext } from "../../utils/GlobalState";
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
+import { useSelector, useDispatch } from 'react-redux';
 
 function ProductItem(item) {
-  const [state, dispatch] = useStoreContext();
+
+  // const [state, dispatch] = useStoreContext();
 
   const {
     image,
@@ -17,6 +19,9 @@ function ProductItem(item) {
   } = item;
 
   const { cart } = state
+
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
 
   const addToCart = () => {
     const itemInCart = cart.find((cartItem) => cartItem._id === _id)
